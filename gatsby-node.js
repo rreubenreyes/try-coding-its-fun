aa3030f1aa14bf1798df2fa60004c5a4b37c5759
Reuben Reyes <reuben.a.reyes@gmail.com>
2018-07-09 23:21:47 -0700

    add page generation to single-post layout

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,60 +3,56 @@ const { createFilePath } = require('gatsby-source-filesystem')
 
 /* query retrieval hook */
 exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
-  /**
-   * @param { object }    node
-   * @param { object }    getNode
-   * @param { object }    boundActionCreators
-   */
-  if (node.internal.type === 'MarkdownRemark') {
-    const { createNodeField } = boundActionCreators
-    const slug = createFilePath({
-      node,
-      getNode,
-      basePath: 'posts', // defined as a resolution in plugins
-    })
-    createNodeField({
-      node,
-      name: 'slug',
-      value: `/posts${slug}`,
-    })
-  }
+	if (node.internal.type === 'MarkdownRemark') {
+		const { createNodeField } = boundActionCreators
+		const slug = createFilePath({
+			node,
+			getNode,
+			basePath: 'posts' // defined as a resolution in plugins
+		})
+		createNodeField({
+			node,
+			name: 'slug',
+			value: `/posts${slug}`
+		})
+	}
 }
 
 /* route and dynamic page generation */
 exports.createPages = ({ graphql, boundActionCreators }) => {
-  const singlePostTemplate = path.resolve('./src/pages/single-post.js')
-  const { createPage } = boundActionCreators
-  return new Promise((resolve, reject) => {
-    // this is an "anonymous query"
-    // data from this query is passed to the target component
-    // then it goes away
-    graphql(`
-      {
-        allMarkdownRemark {
-          edges {
-            node {
-              id
-              fields {
-                slug
-              }
-            }
-          }
-        }
-      }
-    `).then(result => {
-      if (result.errors) reject(result.errors)
-      result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-        createPage({
-          path: node.fields.slug, // define the route
-          component: singlePostTemplate, // define the template
-          context: {
-            // create context
-            slug: node.fields.slug,
-          },
-        })
-      })
-    })
-    resolve()
-  })
+	const singlePostTemplate = path.resolve('./src/layouts/single-post.js')
+	const { createPage } = boundActionCreators
+	return new Promise((resolve, reject) => {
+		// this is an "anonymous query"
+		// data from this query is passed to the target component
+		// then it goes away
+		graphql(`
+			{
+				allMarkdownRemark {
+					edges {
+						node {
+							id
+							fields {
+								slug
+							}
+						}
+					}
+				}
+			}
+		`).then(result => {
+			if (result.errors) reject(result.errors)
+			// create blog posts
+			result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+				createPage({
+					path: node.fields.slug, // define the route
+					component: singlePostTemplate, // define the template
+					context: {
+						// create context
+						slug: node.fields.slug
+					}
+				})
+			})
+		})
+		resolve()
+	})
 }
--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,35 +1,35 @@
 import React, { Component } from 'react'
-import PostDirectory from '../components/Posts/PostDirectory'
+import PostListing from '../components/Posts/PostListing'
 
 export default class Posts extends Component {
-  constructor(props) {
-    super(props)
-  }
-  render() {
-    return (
-      <div>
-        {this.props.data.allMarkdownRemark.edges.map(({ node }) => {
-          return <PostDirectory key={node.id} post={node} />
-        })}
-      </div>
-    )
-  }
+	constructor(props) {
+		super(props)
+	}
+	render() {
+		return (
+			<div>
+				{this.props.data.allMarkdownRemark.edges.map(({ node }) => {
+					return <PostListing key={node.id} post={node} />
+				})}
+			</div>
+		)
+	}
 }
 
 export const query = graphql`
-  query PostsQuery {
-    allMarkdownRemark {
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date(formatString: "MMMM DD, YYYY")
-          }
-          html
-          excerpt
-        }
-      }
-    }
-  }
+	query PostsQuery {
+		allMarkdownRemark {
+			edges {
+				node {
+					id
+					frontmatter {
+						title
+						date(formatString: "MMMM DD, YYYY")
+					}
+					html
+					excerpt
+				}
+			}
+		}
+	}
 `
