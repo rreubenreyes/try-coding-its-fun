59f626b249726111ba21d16668d390eda9033279
Reuben Reyes <reuben.a.reyes@gmail.com>
2018-08-20 00:24:08 -0700

    configure eslint with husky

--- src/data/shortcodes.js
+++ src/data/shortcodes.js
@@ -1,5 +1,5 @@
 const ø = {
-  øCS: `Coming soon!`,
-  øWIP: `Whoops! We're still working on this section. Check back soon!`
+  CS: `Coming soon!`,
+  WIP: `Whoops! We're still working on this section. Check back soon!`,
 }
 export default ø
